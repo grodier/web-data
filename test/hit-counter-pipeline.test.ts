733a16da10d73671099e88a97467048b25b34c9c
grodier <grodier3@gmail.com>
2023-09-21 19:47:06 -0400

    Initial commit

---
+++ test/hit-counter-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HitCounterPipeline from '../lib/hit-counter-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hit-counter-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HitCounterPipeline.HitCounterPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
